cacd987fe6f85464b80204c0ec72dfea459fcfe2
sarim <sarimk80>
2020-02-06 21:21:52 +0500

    volunteer post creation page

--- lib/screens/VolunteerPostSend.dart
+++ lib/screens/VolunteerPostSend.dart
@@ -1,3 +1,7 @@
+import 'package:aghaz/screens/SignInPage.dart';
+import 'package:aghaz/widgets/AgahzTextField.dart';
+import 'package:aghaz/widgets/AghazButton.dart';
+import 'package:aghaz/widgets/AghazCard.dart';
 import 'package:flutter/material.dart';
 
 class VolunteerPostSend extends StatelessWidget {
@@ -5,8 +9,28 @@ class VolunteerPostSend extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("Send"),
+    return Column(
+      children: <Widget>[
+        AghazCard(
+            name: 'UserName',
+            date: 'March 20 2020',
+            title: 'Water',
+            additionalDetail: 'asnajskfnajkfna',
+            detail: 'Karachi,Pakistan Johar bloc 5',
+            imageUrl: 'null',
+            color: Colors.blueAccent),
+        AghazButton(
+            lable: 'Send',
+            onPress: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => SignInPage(),
+                ),
+              );
+            },
+            color: Colors.blueAccent)
+      ],
     );
   }
 }
--- lib/widgets/AgahzTextField.dart
+++ lib/widgets/AgahzTextField.dart
@@ -1,6 +1,8 @@
 import 'package:aghaz/helper/ScreenSize.dart';
 import 'package:flutter/material.dart';
 
+enum TextFieldType { simple, detail ,cutom}
+
 class AghazTextField extends StatelessWidget {
   final IconData icon;
   final String hint;
@@ -8,20 +10,132 @@ class AghazTextField extends StatelessWidget {
   final bool obscure;
   final FocusNode focusNode;
   final FocusNode focusNodeOther;
+  final TextFieldType type;
 
   const AghazTextField(
       {Key key,
       @required this.hint,
-      @required this.icon,
+       this.icon,
       @required this.lable,
       this.obscure = false,
       this.focusNode,
-      this.focusNodeOther})
+      this.focusNodeOther,
+      this.type = TextFieldType.simple})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     ScreenSize().init(context);
+
+    if(type==TextFieldType.cutom){
+      return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        width: ScreenSize.blockSizeHorizontal * 100,
+        height: ScreenSize.blockSizeVertical * 8,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+                color: Theme.of(context).accentColor.withOpacity(0.3),
+                offset: Offset(1.5, 1.5),
+                blurRadius: 5,
+                spreadRadius: 0.2),
+          ],
+        ),
+        child: TextFormField(
+          focusNode: focusNode,
+          onFieldSubmitted: (value) {
+            if (focusNode == focusNodeOther) {
+              FocusScope.of(context).unfocus();
+            } else {
+              focusNode.unfocus();
+              FocusScope.of(context).requestFocus(focusNodeOther);
+            }
+          },
+          obscureText: obscure,
+          style: TextStyle(color: Colors.grey),
+          keyboardType: TextInputType.text,
+          decoration: InputDecoration(
+            labelText: lable,
+            hintText: hint,
+            border: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.white, width: 0),
+              borderRadius: BorderRadius.all(
+                Radius.circular(5),
+              ),
+            ),
+            enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.white, width: 0),
+              borderRadius: BorderRadius.all(
+                Radius.circular(5),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+    }
+
+    if (type == TextFieldType.detail) {
+      return Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Container(
+          width: ScreenSize.blockSizeHorizontal * 100,
+          height: ScreenSize.blockSizeVertical * 20,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            boxShadow: [
+              BoxShadow(
+                  color: Theme.of(context).accentColor.withOpacity(0.3),
+                  offset: Offset(1.5, 1.5),
+                  blurRadius: 5,
+                  spreadRadius: 0.2),
+            ],
+          ),
+          child: TextFormField(
+            maxLines: 4,
+            maxLength: 80,
+            focusNode: focusNode,
+            onFieldSubmitted: (value) {
+              if (focusNode == focusNodeOther) {
+                FocusScope.of(context).unfocus();
+              } else {
+                focusNode.unfocus();
+                FocusScope.of(context).requestFocus(focusNodeOther);
+              }
+            },
+            obscureText: obscure,
+            style: TextStyle(color: Colors.grey),
+            keyboardType: TextInputType.text,
+            //maxLength: 20,
+            decoration: InputDecoration(
+              labelText: lable,
+              hintText: hint,
+              focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.white, width: 0),
+                borderRadius: BorderRadius.all(
+                  Radius.circular(5),
+                ),
+              ),
+              border: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.white, width: 0),
+                borderRadius: BorderRadius.all(
+                  Radius.circular(5),
+                ),
+              ),
+              enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.white, width: 0),
+                borderRadius: BorderRadius.all(
+                  Radius.circular(5),
+                ),
+              ),
+            ),
+          ),
+        ),
+      );
+    }
+
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: Container(
@@ -51,21 +165,22 @@ class AghazTextField extends StatelessWidget {
           style: TextStyle(color: Colors.grey),
           keyboardType: TextInputType.text,
           decoration: InputDecoration(
-              labelText: lable,
-              hintText: hint,
-              prefixIcon: Icon(icon),
-              border: OutlineInputBorder(
-                borderSide: BorderSide(color: Colors.white, width: 0),
-                borderRadius: BorderRadius.all(
-                  Radius.circular(5),
-                ),
+            labelText: lable,
+            hintText: hint,
+            prefixIcon: Icon(icon),
+            border: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.white, width: 0),
+              borderRadius: BorderRadius.all(
+                Radius.circular(5),
               ),
-              enabledBorder: OutlineInputBorder(
-                borderSide: BorderSide(color: Colors.white, width: 0),
-                borderRadius: BorderRadius.all(
-                  Radius.circular(5),
-                ),
-              )),
+            ),
+            enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.white, width: 0),
+              borderRadius: BorderRadius.all(
+                Radius.circular(5),
+              ),
+            ),
+          ),
         ),
       ),
     );
